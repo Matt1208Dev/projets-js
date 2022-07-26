819e7cccd1125e39cadb916b5e5a58859c27204c
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2022-07-27 01:51:07 +0200

    partie JS terminée.

--- 1. IMC/app.js
+++ 1. IMC/app.js
@@ -7,4 +7,35 @@ const BMIData = [
   { name: "Obésité morbide", color: "purple", range: 40 },
 ];
 
-// IMC = poids en kg / taille² en m
+const height = document.querySelector('#height');
+const weight = document.querySelector('#weight');
+const submit = document.querySelector('button');
+const result = document.querySelector('.result-title');
+const message = document.querySelector('.result-text');
+
+submit.addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  // Valider la saisie
+  if ((height.value === "" || weight.value === "") || (height.value <= 0 || weight.value <= 0)) {
+    result.innerHTML = "Oups !";
+    message.innerHTML = "Remplissez correctement les champs";
+  } else {
+    
+    // Calculer l'IMC
+    // IMC = poids en kg / taille² en m
+    IMC = Math.round((weight.value / ((height.value / 100) * (height.value / 100 ))) * 10) / 10;
+
+    // Déterminer le rang à partir de BMIData et afficher le résultat à l'utilisateur.
+    for (rank of BMIData) {
+      if ((IMC >= rank.range[0] && IMC <= rank.range[1]) || IMC > rank.range) {
+        
+        result.style.color = rank.color;
+        result.innerHTML = IMC;
+        message.innerHTML = `Résultat : ${rank.name}`;
+      }
+    }
+  }
+
+
+});
