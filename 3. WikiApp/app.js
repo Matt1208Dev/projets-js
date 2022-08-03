4ee49811ce9ec5ae9fa4725ec59fb38a73a923dd
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2022-08-03 17:31:38 +0200

    fix: correctifs CSS et JS +ajout messages d'erreur

--- 3. WikiApp/app.js
+++ 3. WikiApp/app.js
@@ -11,18 +11,20 @@ function handleForm(e) {
 
     loader.style.display = "flex";
 
-    if (searchInput) {
+    if (searchInput !== "") {
 
         fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`)
             .then(response => response.json())
             .then(data => showResults(data))
             .catch(function (error) {
                 console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+                errorMsg.textContent = "Problème de connexion réseau";
             });
     } else {
         const results = document.querySelector('.results');
         eraseResults();
         loader.style.display = "none";
+        return;
     }
 
     // loader.style.display = "none";
@@ -32,6 +34,11 @@ function showResults(data) {
 
     const arrResults = data.query.search;
     const results = document.querySelector('.results');
+    const errorMsg = document.querySelector('.error-msg');
+
+    if (arrResults.length === 0)  {
+        errorMsg.textContent = "Aucun résultat à afficher...";
+    }
     // Effacement des résultats
     eraseResults();
 
@@ -49,7 +56,7 @@ function showResults(data) {
         cardText.className = 'card-text';
 
         // Intégration des contenus
-        cardTitle.innerHTML = `<a href="https://en.wikipedia.org/?curid=${arrResults[index].pageid}">${arrResults[index].title}</a>`;
+        cardTitle.innerHTML = `<a href="https://en.wikipedia.org/?curid=${arrResults[index].pageid}" target="_blank">${arrResults[index].title}</a>`;
         cardURL.textContent = `https://en.wikipedia.org/?curid=${arrResults[index].pageid}`;
         cardText.innerHTML = arrResults[index].snippet;
 
