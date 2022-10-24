56eca72a33c1a90b90378d27b5f3c3e24f402299
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2022-10-25 00:06:34 +0200

    TypingGame: Base HTML + CSS + JS: fonction timer()

--- 18. TypingGame/app.js
+++ 18. TypingGame/app.js
@@ -1,1 +1,14 @@
 const APIEndpoint = "http://api.quotable.io/random";
+const time = document.querySelector('.time');
+let timeMax = 60;
+
+function timer() {
+    let remainingTime = setInterval(() => {
+        timeMax--;
+        time.textContent = timeMax;
+        if (timeMax === 0) {
+            clearInterval(remainingTime);
+        }
+    }, 1000);
+}
+timer();
